32a49149b9a235ab1a9598a1b51c3dca175950ce
Rosmontis <58333458+Minecrafter-Pythoner@users.noreply.github.com>
2023-07-05 11:40:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,7 +34,9 @@ int main(int argc, char* argv[]) {
             else if (strcasecmp(operation.c_str(), "undoTask") == 0) userOption = 6;
             switch(userOption){
                 case 0: cout << "Command unknown. Plese check your input." << endl; exit(404); break;
-                case 1:
+                case 1: {
+                    
+                }
             }
         }
     }
